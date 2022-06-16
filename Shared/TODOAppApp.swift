54871d19f4a4700ccc9acd0aa3cf8224e549833c
Seyyed Alireza <alireza79.ghazanfari@gmail.com>
2022-06-16 16:13:53 -0700

    Initial Commit

---
+++ Shared/TODOAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODOAppApp.swift
+//  Shared
+//
+//  Created by Macvps on 6/16/22.
+//
+
+import SwiftUI
+
+@main
+struct TODOAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
