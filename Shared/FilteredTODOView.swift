02639ba3d92a3ee28c21b35970eb3ab5b5d6f85a
Seyyed Alireza <alireza79.ghazanfari@gmail.com>
2022-06-21 10:17:00 -0700

    feature: add sort for list

--- Shared/FilteredTODOView.swift
+++ Shared/FilteredTODOView.swift
@@ -13,10 +13,30 @@ struct FilteredTODOView: View {
     @State var selectedItems: [TODO]
     @State var selectedDate: Date
     
+    @State var sortBy = "date"
+    @State var isAscending = false
+    @State var showingSheet = false
+    
     init(items firsts: Binding<[TODO]>, selected selectedItems: [TODO]) {
         _selectedDate = State(initialValue: Date())
         _items = firsts
         _selectedItems = State(initialValue: selectedItems)
+    }
+    
+    func sortList(by sortBy: String, ord isAscending: Bool){
+        switch sortBy{
+            case "date":
+            selectedItems.sort  {
+                    isAscending ? $0.date < $1.date : $0.date > $1.date
+                }
+            case "name":
+            selectedItems.sort {
+                    isAscending ? $0.title < $1.title : $0.title > $1.title
+                }
+        default:
+            break
+        }
+        
     }
     
     var body: some View {
@@ -27,6 +47,29 @@ struct FilteredTODOView: View {
                         Calendar.current.dateComponents([.day, .month, .year], from: selectedDate) == Calendar.current.dateComponents([.day, .month, .year], from: obj.date)
                     }
                 })
+            HStack {
+                Button("Sort by") {
+                    showingSheet.toggle()
+                }.sheet(isPresented: $showingSheet){
+                    Text("date asc").onTapGesture {
+                        sortList(by: "date", ord: true)
+                        showingSheet.toggle()
+                    }
+                    Text("date des").onTapGesture {
+                        sortList(by: "date", ord: false)
+                        showingSheet.toggle()
+                    }
+                    Text("name asc").onTapGesture {
+                        sortList(by: "date", ord: true)
+                        showingSheet.toggle()
+                    }
+                    Text("name des").onTapGesture {
+                        sortList(by: "date", ord: false)
+                        showingSheet.toggle()
+                    }
+                }
+
+            }
             List{
                 ForEach(selectedItems){item in
                     VStack(alignment: .leading){
@@ -48,7 +91,10 @@ struct FilteredTODOView: View {
                     
                 }
             }
+            
         }
                 
     }
+    
 }
+
